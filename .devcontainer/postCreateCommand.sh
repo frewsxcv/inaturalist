70008e2b9b1ab18b9bbaf77b89f296aaf96ce346
Corey Farwell <coreyf@rwell.org>
2023-05-07 18:14:22 +0000

    tmp

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -3,13 +3,3 @@
 # Exit script on error
 set -e
 
-rvm install $CODESPACE_VSCODE_FOLDER
-
-source $NVM_DIR/nvm.sh
-nvm install
-
-cp docker-compose.override.yml.example docker-compose.override.yml
-
-docker-compose build
-
-bin/setup ruby
