1c3168ff085792ec0731a25ce7275bb0f122eaa7
Corey Farwell <coreyf@rwell.org>
2023-12-17 13:48:08 -0500

    create command

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-# Exit script on error
-set -e
+# -e Exit script on error
+# -x Print commands
+set -ex
 
 sudo apt-get update
 
 sudo apt-get install -y proj-bin postgresql-client
 
-docker-compose pull
+# docker-compose pull
 
 . $RVM_PATH/scripts/rvm
 rvm install $(cat .ruby-version)
@@ -16,9 +17,9 @@ rvm use $(cat .ruby-version)
 . $NVM_DIR/nvm.sh
 nvm install
 
-cp docker-compose.override.yml.example docker-compose.override.yml
+# cp docker-compose.override.yml.example docker-compose.override.yml
 
 # `bin/setup ruby` requires services to be started
-docker-compose up -d
+# docker-compose up -d
 
 bin/setup ruby
